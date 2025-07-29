553c35b411d32e868a983e6df2477c6f284e0af0
CodeWithJex <erlandorizki7166@gmail.com>
2025-07-29 10:01:27 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,104 @@
+//Part 1
+// void main(){
+//     print ("Selamat Pagi Dunia Tipu Tipu");
+// }
+
+//Part 2
+// var x = 42;
+// int y = 100;
+// double pi = 3.14;
+// String s = 'halo';
+// bool b = true;
+// final now = DateTime.now();
+// const pic = 3.14159;
+
+//Part 3
+// import 'dart:io';
+// void main (){
+//   print("Masukan nama:");
+//   String? name = stdin.readLineSync();
+
+//   print("Masukan umur:");
+//   int age = int.parse(stdin.readLineSync()!);
+
+//   print('Halo $name, umurmu tahun depan adalah ${age + 1} tahun.');
+
+// }
+
+//part 4
+// void main() {
+// int nilai = 85;
+//   if (nilai >= 90){
+//     print("Nilai A");
+//   } else if (nilai >= 80) {
+//     print("Nilai B");
+//   } else if (nilai >= 70) {
+//     print("Nilai C");
+//   } else {
+//     print("Nilai D atau E");
+//   }
+// }
+
+// //Part 5
+// void main(){ 
+// for (int i = 1; i <= 5; i++) {
+//   print("Halo ke -$i");
+// }
+// }
+
+//Part 6
+// void main(){
+// int i = 1;
+// do {
+//   print("Angka $i");
+//   i++;
+// } while (i <= 3);
+// }
+
+//Part 7
+// void main(){
+// int i = 1;
+// while (i <= 5) {
+//   print("Perulangan ke-$i");
+//   i++;
+// }
+// }
+
+
+//Latihan Perhitungan 1
+// import 'dart:io';
+// void main (){
+//   print("Masukan Panjangnya:");
+//   int panjang = int.parse(stdin.readLineSync()!);
+
+//   print("Masukan Lebarnya:");
+//   int lebar = int.parse(stdin.readLineSync()!);
+
+//   print('Hasil Luas = ${panjang * lebar}');
+//   print('Hasil Keliling = ${2 * panjang * lebar}');
+//   // print('Halo $name, umurmu tahun depan adalah ${age + 1} tahun.');
+// }
+
+
+// //      latihan 2
+// import 'dart:io';
+// void main() {
+//   print("masukan angka");
+//   int angka = int.parse(stdin.readLineSync()!);
+
+//   if (angka % 2 == 0) {
+//     print("angka genap");
+//   } else {
+//     print("angka ganjil");
+//   }
+// }
+
+
+// //      latihan 3
+// import 'dart:io';
+// void main() {
+//   print("masukan luas lingkaran");
+//   int ruas = int.parse(stdin.readLineSync()!);
+
+//   print('panjang kotak adalah ${3.14 * ruas*ruas}cm');
+// }
